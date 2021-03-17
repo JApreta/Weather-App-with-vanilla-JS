ba478d06adf1b58b5c64afd04e6ebf9e4d5b58f6
JAntonio <judithantonio@email.arizona.edu>
2021-03-16 22:18:44 -0700

    changed background & added forecast

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const apiKey = "3a2f76834c807537970303e3dab30fe7";
-let city;
+let city = "Tucson";
 let temp = document.querySelector("#temperature");
 let toCel = document.querySelector("#showTempInC");
 let tofahrenheit = document.querySelector("#showTempInF");
@@ -26,7 +26,7 @@ function showTemperature(response) {
     document.querySelector("#humidity").innerHTML = ` Humidity: ${response.data.main.humidity}%`;
     document.querySelector("#wind").innerHTML = ` Wind: ${Math.round(response.data.wind.speed)} km/h`;
     document.querySelector("#description").innerHTML = `${response.data.weather[0].description}`;
-    showCurrentTime((response.data.dt * 1000));
+    showCurrentTime();
 
     mainImage.setAttribute(
         "src",
@@ -35,10 +35,52 @@ function showTemperature(response) {
     mainImage.setAttribute("alt", response.data.weather[0].description);
 }
 
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours().toString().padStart(2, '0');
+    let minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
+
+function dispalyForecast(response) {
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+    //console.log(response.data.list[0]);
+
+    for (let index = 0; index < 6; index++) {
+        forecast = response.data.list[index];
+        forecastElement.innerHTML += `
+        <div class="col-2">
+          <h6>
+            ${formatHours(forecast.dt * 1000)}
+          </h6>
+          <img
+            src="icons/${forecast.weather[0].icon}.svg"
+          />
+          <div class="weather-forecast-temperature">
+           ${forecast.pop*100}% <br>
+            <strong>
+              ${Math.round(forecast.main.temp_max)}°
+            </strong>
+            ${Math.round(forecast.main.temp_min)}°
+          
+          </div>
+        </div>
+      `;
+    }
+}
+
+
 function getCityLiveData(city) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showTemperature).catch((error) => {
         alert("City Not Found");
+    });
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(dispalyForecast).catch((error) => {
+
     });
 }
 
@@ -63,23 +105,26 @@ function showCurrentLocationLiveData() {
     });
 }
 
-showCurrentLocationLiveData();
+
 document.querySelector('#currentLocation').addEventListener('click', showCurrentLocationLiveData);
 
-function showCurrentTime(timestamp) {
-    let currentDate = new Date(timestamp);
-    document.querySelector("#date_time").innerHTML = `${
-  weekdays[currentDate.getDay()]
-} ${currentDate.getHours()}:${
-  currentDate.getMinutes() < 10 ? "0" : ""
-}${currentDate.getMinutes()}`;
+function showCurrentTime() {
+    let currentDate = new Date();
+    document.querySelector("#date_time").innerHTML = `Last Update at ${
+        weekdays[currentDate.getDay()]} 
+  ${currentDate.getHours().toString().padStart(2, '0')}:${
+  currentDate.getMinutes().toString().padStart(2, '0')
+}`;
 }
 document.querySelector("#searchCity").addEventListener("submit", function(e) {
     e.preventDefault();
     city = document.querySelector("#city").value;
+    document.querySelector("#city").value = '';
     getCityLiveData(city);
 
 });
+
+getCityLiveData(city);
 
 
 tofahrenheit.addEventListener("click", function(e) {
